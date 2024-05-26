92ad1bec55744252f80d7e3d99c4879f92010ee2
youcef12091997 <youcef12091997@gmail.com>
2024-05-26 19:00:35 +0200

    add medecins list

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,38 +14,39 @@ class LoginController extends Controller
     }
 
     public function loginPost(Request $request) {
+        // dd($request);
         $credentials = $request->only('nom_prenom', 'password');
-    
+        //  dd($credentials);
         if (Auth::attempt($credentials)) {
             // Vérifier si le paramètre de restauration de session est présent
             if ($request->has('restore_session')) {
                 // Récupérer les données utilisateur avant déconnexion
                 $userDataBeforeLogout = Session::get('user_data_before_logout');
-    
+
                 if ($userDataBeforeLogout) {
                     // Réappliquer les données de session précédentes
                     Session::put($userDataBeforeLogout);
-    
+
                     // Supprimer les données stockées avant déconnexion
                     Session::forget('user_data_before_logout');
                 }
             }
-            
+
             return redirect()->intended($this->redirectTo());
         }
-    
+
         return redirect()->back()->withInput()->withErrors(['login_error' => 'Identifiants invalides']);
     }
-    
+
         public function logout(Request $request)
         {
             Auth::logout();
-    
+
             $request->session()->invalidate();
-    
+
             return redirect('/');
         }
-    
+
         private function redirectTo()
         {
             switch(auth()->user()->role) {
